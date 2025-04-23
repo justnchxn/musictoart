a596d1a017543203e4cdd515c5a719fc753e59c5
Justin Chan <justnchxn@gmail.com>
2025-04-23 11:02:16 +0100

    time frames for track history

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ sp = spotipy.Spotify(
         scope = ["user-top-read", "user-library-read", "playlist-read-private", "playlist-read-collaborative"]
     )
 )
+
+st.set_page_config(page_title = "Spotify Music Visualiser", page_icon = "musical_note")
+st.title("Spotify Music Visualiser")
+st.write("Converts Your Music Tastes Into Art")
+
+top_tracks_short = sp.current_user_top_tracks(limit=10, time_range="short_term")
+top_tracks_medium = sp.current_user_top_tracks(limit=10, time_range="medium_term")
+top_tracks_long = sp.current_user_top_tracks(limit=10, time_range="long_term")

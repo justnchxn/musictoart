4b93c690cf17b1a80c8479e2074d482bb0a131bf
Justin Chan <justnchxn@gmail.com>
2025-04-23 10:55:08 +0100

    requirements + spotify api scopes

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import streamlit as st
+import pandas as pd
 import os
-import base64
-from requests import post
-import json
-
-load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+redirect_url = "http://localhost:5000"
 
-print(client_id, client_secret)
-
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded" 
-    }
-    data = {"grant_type": "client_credentials"}     
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
-
-def get_auth_header(token):
-    return {"Authorisation": "Bearer " + token}
-
-
-token = get_token()
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id = client_id,
+        client_secret = client_secret,
+        redirect_url = redirect_url,
+        scope = ["user-top-read", "user-library-read", "playlist-read-private", "playlist-read-collaborative"]
+    )
+)
